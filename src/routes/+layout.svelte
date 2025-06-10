8f925391b1bf63d754d478dfb1310764d069b0bf
Hannes Rüger <hannes@rueger.events>
2025-06-10 21:27:12 +0200

    feat(ui): add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,17 @@
     import "../app.css";
 </script>
 
-<slot />
+<main>
+    <slot />
+</main>
+
+<footer class="w-100 bg-light text-center p-4">
+    &copy; 2025 Hannes Rüger, based on work by Jan Fröhlich<br />
+    <i>Built with ❤️, Svelte & Bootstrap - Open Source on <a target="_blank" href="https://github.com/hrueger/camera-characterizer">GitHub</a></i>
+</footer>
+
+<style>
+    main {
+        min-height: calc(100vh - 6rem);
+    }
+</style>
