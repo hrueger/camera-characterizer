6328403d2e5bf82b61fce166edc2bd42cc189b16
Hannes Rüger <hannes@rueger.events>
2025-06-18 08:19:10 +0200

    fix: typo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -82,7 +82,7 @@
 <div class="container-fluid py-2">
     <div class="row">
         <div class="col-12">
-            <h1 class="mb-3">Chamera Characterization Tool</h1>
+            <h1 class="mb-3">Camera Characterization Tool</h1>
             <div class="card mb-3 p-3">
                 <div class="row">
                     <div class="col">
